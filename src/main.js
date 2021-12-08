cb3306e2f2340388ce213d3e5da9118a8a7be4be
liaozhongxun <869664233@qq.com>
2021-12-08 16:36:03 +0800

    add note

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+// px 转 vw 
+// 1.npm 下载 postcss-px-to-viewport
+// 2.vue.config.js 或 postcss.config.js 配置 postcss-px-to-viewport
+// 3.项目正常使用px项目解析后自动转相应的vw
+
 createApp(App).use(store).use(router).mount("#app");
